63d0c2c2999b45e5f181f9bce4093940ca9dfb17
Muhammad Jamil <jamil138.amin@gmail.com>
2024-05-07 17:50:32 +0500

    My Second App

--- Lab 24 - Laravel Part II/MySecondApp/routes/web.php
+++ Lab 24 - Laravel Part II/MySecondApp/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\frontend\ContactController;
 use App\Http\Controllers\frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
+//Routes
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about-us', [AboutController::class, 'index']);
 Route::get('/contact-us', [ContactController::class, 'index']);
